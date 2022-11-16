9ae9520e440edf9ddff293c5057d3a1e79e439bd
JJ Case <jjcasegaming@yahoo.com>
2022-11-15 17:55:53 -0800

    fixed white widow

---
+++ configs/ottos.lua
@@ -0,0 +1,85 @@
+
+
+-- 12 created by JJ_Case
+Config.DoorList['ottos-12'] = {
+    doorRate = 1.0,
+    doorType = 'garage',
+    distance = 4,
+    objName = 270330101,
+    objCoords = vec3(823.839050, -820.270996, 27.395458),
+    fixText = false,
+    authorizedJobs = { ['ottos'] = 0 },
+    objYaw = 270.00012207031,
+    locked = true,
+    doorLabel = 'garagedoor1',
+}
+
+-- 13 created by JJ_Case
+Config.DoorList['ottos-13'] = {
+    doorRate = 1.0,
+    doorType = 'garage',
+    distance = 4,
+    objName = 270330101,
+    objCoords = vec3(823.838989, -812.950195, 27.395458),
+    fixText = false,
+    authorizedJobs = { ['ottos'] = 0 },
+    objYaw = 270.00012207031,
+    locked = true,
+    doorLabel = 'garagedoor2',
+}
+
+-- 14 created by JJ_Case
+Config.DoorList['ottos-14'] = {
+    doorRate = 1.0,
+    doorType = 'garage',
+    distance = 4,
+    objName = 270330101,
+    objCoords = vec3(823.838989, -805.582031, 27.395458),
+    fixText = false,
+    authorizedJobs = { ['ottos'] = 0 },
+    objYaw = 270.00012207031,
+    locked = true,
+    doorLabel = 'garagedoor3',
+}
+
+-- 15 created by JJ_Case
+Config.DoorList['ottos-15'] = {
+    doorRate = 1.0,
+    doorType = 'door',
+    distance = 2,
+    objName = -147325430,
+    objCoords = vec3(829.966248, -824.674744, 26.482376),
+    fixText = false,
+    authorizedJobs = { ['ottos'] = 0 },
+    objYaw = 180.00003051758,
+    locked = true,
+    doorLabel = 'office1',
+}
+
+-- 16 created by JJ_Case
+Config.DoorList['ottos-16'] = {
+    doorRate = 1.0,
+    doorType = 'door',
+    distance = 2,
+    objName = -147325430,
+    objCoords = vec3(837.653137, -821.313477, 26.482376),
+    fixText = false,
+    authorizedJobs = { ['ottos'] = 0 },
+    objYaw = 270.00003051758,
+    locked = true,
+    doorLabel = 'office2',
+}
+
+-- 18 created by JJ_Case
+Config.DoorList['ottos-18'] = {
+    doorRate = 1.0,
+    doorType = 'door',
+    distance = 2,
+    objName = 263193286,
+    objCoords = vec3(840.532104, -820.562439, 26.482372),
+    fixText = false,
+    authorizedJobs = { ['ottos'] = 0 },
+    objYaw = 0.0,
+    locked = true,
+    doorLabel = 'office3',
+}
---
+++ configs/whitewidow.lua
@@ -0,0 +1,99 @@
+
+
+-- 1 created by JJ_Case
+Config.DoorList['whitewidow-1'] = {
+    doorRate = 1.0,
+    doors = {
+        {objName = -1226987899, objYaw = 114.99998474121, objCoords = vec3(201.687973, -240.668671, 54.235588)},
+        {objName = -1226987899, objYaw = 295.0, objCoords = vec3(200.584976, -238.315170, 54.235588)}
+    },
+    doorLabel = 'front doors',
+    doorType = 'double',
+    distance = 2,
+    locked = true,
+    authorizedJobs = { ['whitewidow'] = 0 },
+}
+
+-- 2 created by JJ_Case
+Config.DoorList['whitewidow-2'] = {
+    doorRate = 1.0,
+    doorType = 'garage',
+    distance = 5,
+    objName = 81325078,
+    objCoords = vec3(194.575684, -267.622894, 50.617069),
+    fixText = false,
+    authorizedJobs = { ['whitewidow'] = 0 },
+    objYaw = 70.000007629395,
+    locked = true,
+    doorLabel = 'garagedoor',
+}
+
+-- 3 created by JJ_Case
+Config.DoorList['whitewidow-3'] = {
+    doorRate = 1.0,
+    doorType = 'door',
+    distance = 2,
+    objName = -2051651622,
+    objCoords = vec3(186.794159, -236.938354, 54.221138),
+    fixText = false,
+    authorizedJobs = { ['whitewidow'] = 0 },
+    objYaw = 69.999992370605,
+    locked = true,
+    doorLabel = 'office door1',
+}
+
+-- 4 created by JJ_Case
+Config.DoorList['whitewidow-4'] = {
+    doorRate = 1.0,
+    doorType = 'door',
+    distance = 2,
+    objName = -2051651622,
+    objCoords = vec3(185.367142, -244.162537, 54.219982),
+    fixText = false,
+    authorizedJobs = { ['whitewidow'] = 0 },
+    objYaw = 159.99998474121,
+    locked = true,
+    doorLabel = 'office door2',
+}
+
+-- 5 created by JJ_Case
+Config.DoorList['whitewidow-5'] = {
+    doorRate = 1.0,
+    doorType = 'door',
+    distance = 2,
+    objName = -538477509,
+    objCoords = vec3(182.345337, -243.086380, 54.218788),
+    fixText = false,
+    authorizedJobs = { ['whitewidow'] = 0 },
+    objYaw = 159.99998474121,
+    locked = true,
+    doorLabel = 'office door3',
+}
+
+-- 6 created by JJ_Case
+Config.DoorList['whitewidow-6'] = {
+    doorRate = 1.0,
+    doorType = 'door',
+    distance = 2,
+    objName = 47003473,
+    objCoords = vec3(182.112000, -246.814117, 49.806992),
+    fixText = false,
+    authorizedJobs = { ['whitewidow'] = 0 },
+    objYaw = 250.00003051758,
+    locked = true,
+    doorLabel = 'office door4',
+}
+
+-- 7 created by JJ_Case
+Config.DoorList['whitewidow-7'] = {
+    doorRate = 1.0,
+    doorType = 'sliding',
+    distance = 2,
+    objName = 161378502,
+    objCoords = vec3(174.923813, -237.066940, 49.060406),
+    fixText = false,
+    authorizedJobs = { ['whitewidow'] = 0 },
+    objYaw = 70.0,
+    locked = true,
+    doorLabel = 'grow room',
+}
