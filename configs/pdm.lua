6ffcb3dd85b2d23d26a735afbc49c0ef1a186c02
jj_case-gaming <jjcasegaming@yahoo.com>
2023-01-02 14:13:10 -0800

    pdm

---
+++ configs/pdm.lua
@@ -0,0 +1,57 @@
+
+
+-- pdm1 created by JJ_Case
+Config.DoorList['pdm-pdm1'] = {
+    doorLabel = 'pdm office 1',
+    locked = true,
+    doorType = 'door',
+    distance = 2,
+    fixText = false,
+    objCoords = vec3(-32.642670, -1108.559204, 27.424587),
+    objYaw = 249.99945068359,
+    objName = 2089009131,
+    doorRate = 1.0,
+    authorizedJobs = { ['pdm'] = 0 },
+}
+
+-- garagedoor created by JJ_Case
+Config.DoorList['pdm-garagedoor'] = {
+    distance = 5,
+    objYaw = 159.99942016602,
+    objCoords = vec3(-21.511593, -1089.399414, 28.113455),
+    doorType = 'garage',
+    authorizedJobs = { ['pdm'] = 0 },
+    doorLabel = 'garagedoor1',
+    fixText = false,
+    objName = 1010499530,
+    locked = true,
+    doorRate = 1.0,
+}
+
+-- officedoor2 created by JJ_Case
+Config.DoorList['pdm-officedoor2'] = {
+    distance = 3,
+    objYaw = 249.99945068359,
+    objCoords = vec3(-27.621048, -1094.762817, 27.424587),
+    doorType = 'door',
+    authorizedJobs = { ['pdm'] = 0 },
+    doorLabel = 'officedoor2',
+    fixText = false,
+    objName = 2089009131,
+    locked = true,
+    doorRate = 1.0,
+}
+
+-- officedoor3 created by JJ_Case
+Config.DoorList['pdm-officedoor3'] = {
+    distance = 3,
+    objYaw = 69.999412536621,
+    objCoords = vec3(-30.428459, -1102.470215, 27.424587),
+    doorType = 'door',
+    authorizedJobs = { ['pdm'] = 0 },
+    doorLabel = 'officedoor3',
+    fixText = false,
+    objName = 2089009131,
+    locked = true,
+    doorRate = 1.0,
+}
