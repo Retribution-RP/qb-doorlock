563443d538b9cb6ff2037c24296742bab457664e
jj_case-gaming <jjcasegaming@yahoo.com>
2023-01-05 11:07:38 -0800

    uwu

---
+++ configs/beanmachine.lua
@@ -0,0 +1,29 @@
+
+
+-- bean1 created by JJ_Case
+Config.DoorList['beanmachine-bean1'] = {
+    doors = {
+        {objName = -747011272, objYaw = 69.999992370605, objCoords = vec3(115.375694, -1037.655029, 29.348320)},
+        {objName = -1182160879, objYaw = 250.00004577637, objCoords = vec3(114.562912, -1039.888184, 29.348320)}
+    },
+    authorizedJobs = { ['bean'] = 0 },
+    doorLabel = 'bean1',
+    locked = true,
+    doorType = 'double',
+    doorRate = 1.0,
+    distance = 3,
+}
+
+-- bean2 created by JJ_Case
+Config.DoorList['beanmachine-bean2'] = {
+    locked = true,
+    doorType = 'door',
+    distance = 3,
+    authorizedJobs = { ['bean'] = 0 },
+    fixText = false,
+    objName = 494354570,
+    doorLabel = 'bean2',
+    objYaw = 340.00003051758,
+    objCoords = vec3(128.213394, -1029.455322, 29.261801),
+    doorRate = 1.0,
+}
---
+++ configs/uwucafe.lua
@@ -0,0 +1,71 @@
+
+
+-- uwu1 created by JJ_Case
+Config.DoorList['uwucafe-uwu1'] = {
+    doors = {
+        {objName = 526179188, objYaw = 0.0, objCoords = vec3(-581.667786, -1069.627197, 22.489748)},
+        {objName = -69331849, objYaw = 0.0, objCoords = vec3(-580.361084, -1069.627197, 22.489748)}
+    },
+    authorizedJobs = { ['uwu'] = 0 },
+    doorLabel = 'uwu1',
+    locked = true,
+    doorType = 'double',
+    doorRate = 1.0,
+    distance = 3,
+}
+
+-- uwu2 created by JJ_Case
+Config.DoorList['uwucafe-uwu2'] = {
+    locked = true,
+    doorType = 'door',
+    distance = 3,
+    authorizedJobs = { ['uwu'] = 0 },
+    fixText = false,
+    objName = -1283712428,
+    doorLabel = 'uwu2',
+    objYaw = 89.999977111816,
+    objCoords = vec3(-587.340027, -1051.899414, 22.413006),
+    doorRate = 1.0,
+}
+
+-- uwu5 created by JJ_Case
+Config.DoorList['uwucafe-uwu5'] = {
+    locked = true,
+    doorType = 'door',
+    distance = 3,
+    authorizedJobs = { ['uwu'] = 0 },
+    fixText = false,
+    objName = 1099436502,
+    doorLabel = 'uwu5',
+    objYaw = 269.99996948242,
+    objCoords = vec3(-600.888611, -1055.131470, 22.713028),
+    doorRate = 1.0,
+}
+
+-- uwu6 created by JJ_Case
+Config.DoorList['uwucafe-uwu6'] = {
+    locked = true,
+    doorType = 'garage',
+    distance = 3,
+    authorizedJobs = { ['uwu'] = 0 },
+    fixText = false,
+    objName = 522844070,
+    doorLabel = 'uwu6',
+    objYaw = 270.00003051758,
+    objCoords = vec3(-600.910583, -1059.217651, 21.721432),
+    doorRate = 1.0,
+}
+
+-- uwu8 created by JJ_Case
+Config.DoorList['uwucafe-uwu8'] = {
+    locked = true,
+    doorType = 'door',
+    distance = 3,
+    authorizedJobs = { ['uwu'] = 0 },
+    fixText = false,
+    objName = 2089009131,
+    doorLabel = 'uwu8',
+    objYaw = 0.0,
+    objCoords = vec3(-571.792664, -1057.388428, 26.767965),
+    doorRate = 1.0,
+}
