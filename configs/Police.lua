f8e3cedd5013fcaf24990b884b0f4ab4265d6117
jj_case-gaming <jjcasegaming@yahoo.com>
2023-01-04 18:27:45 -0800

    police

---
+++ configs/Police.lua
@@ -0,0 +1,380 @@
+
+
+-- door1 created by JJ_Case
+Config.DoorList['Police-door1'] = {
+    authorizedJobs = { ['police'] = 0 },
+    distance = 3,
+    doorRate = 1.0,
+    doorLabel = 'door1',
+    doors = {
+        {objName = -1547307588, objYaw = 180.00001525879, objCoords = vec3(443.061768, -998.746216, 30.815304)},
+        {objName = -1547307588, objYaw = 0.0, objCoords = vec3(440.739197, -998.746216, 30.815304)}
+    },
+    locked = true,
+    doorType = 'double',
+}
+
+
+-- pddoor2 created by JJ_Case
+Config.DoorList['police-pddoor2'] = {
+    objCoords = vec3(440.520081, -977.601074, 30.823193),
+    distance = 3,
+    objName = -1406685646,
+    authorizedJobs = { ['police'] = 0 },
+    doorType = 'door',
+    doorRate = 1.0,
+    objYaw = 0.0,
+    fixText = false,
+    locked = true,
+    doorLabel = 'pddoor2',
+}
+
+-- pddoor3 created by JJ_Case
+Config.DoorList['police-pddoor3'] = {
+    objCoords = vec3(440.520081, -986.233459, 30.823193),
+    distance = 3,
+    objName = -96679321,
+    authorizedJobs = { ['police'] = 0 },
+    doorType = 'door',
+    doorRate = 1.0,
+    objYaw = 180.00001525879,
+    fixText = false,
+    locked = true,
+    doorLabel = 'pddoor3',
+}
+
+-- pddoor4 created by JJ_Case
+Config.DoorList['police-pddoor4'] = {
+    objCoords = vec3(479.750732, -999.629028, 30.789167),
+    distance = 3,
+    objName = -692649124,
+    authorizedJobs = { ['police'] = 0 },
+    doorType = 'door',
+    doorRate = 1.0,
+    objYaw = 89.999977111816,
+    fixText = false,
+    locked = true,
+    doorLabel = 'pddoor4',
+}
+
+-- pddoor5 created by JJ_Case
+Config.DoorList['police-pddoor5'] = {
+    objCoords = vec3(487.437836, -1000.189270, 30.786972),
+    distance = 3,
+    objName = -692649124,
+    authorizedJobs = { ['police'] = 0 },
+    doorType = 'door',
+    doorRate = 1.0,
+    objYaw = 181.28001403809,
+    fixText = false,
+    locked = true,
+    doorLabel = 'pddoor5',
+}
+
+-- pddoor6 created by JJ_Case
+Config.DoorList['police-pddoor6'] = {
+    objCoords = vec3(452.300507, -1000.771667, 26.696609),
+    distance = 5,
+    objName = 2130672747,
+    authorizedJobs = { ['police'] = 0 },
+    doorType = 'garage',
+    doorRate = 1.0,
+    objYaw = 0.0,
+    fixText = false,
+    locked = true,
+    doorLabel = 'pddoor6',
+}
+
+-- pddoor7 created by JJ_Case
+Config.DoorList['police-pddoor7'] = {
+    objCoords = vec3(431.411926, -1000.771667, 26.696609),
+    distance = 5,
+    objName = 2130672747,
+    authorizedJobs = { ['police'] = 0 },
+    doorType = 'garage',
+    doorRate = 1.0,
+    objYaw = 0.0,
+    fixText = false,
+    locked = true,
+    doorLabel = 'pddoor7',
+}
+
+-- pddoor8 created by JJ_Case
+Config.DoorList['police-pddoor8'] = {
+    objCoords = vec3(464.159058, -974.665588, 26.370705),
+    distance = 3,
+    objName = 1830360419,
+    authorizedJobs = { ['police'] = 0 },
+    doorType = 'door',
+    doorRate = 1.0,
+    objYaw = 269.79000854492,
+    fixText = false,
+    locked = true,
+    doorLabel = 'pddoor8',
+}
+
+-- pddoor9 created by JJ_Case
+Config.DoorList['police-pddoor9'] = {
+    objCoords = vec3(464.156555, -997.509277, 26.370705),
+    distance = 3,
+    objName = 1830360419,
+    authorizedJobs = { ['police'] = 0 },
+    doorType = 'door',
+    doorRate = 1.0,
+    objYaw = 89.870010375977,
+    fixText = false,
+    locked = true,
+    doorLabel = 'pddoor9',
+}
+
+-- pddoor10 created by JJ_Case
+Config.DoorList['police-pddoor10'] = {
+    authorizedJobs = { ['police'] = 0 },
+    doorType = 'double',
+    doorRate = 1.0,
+    doorLabel = 'pddoor10',
+    distance = 3,
+    doors = {
+        {objName = -288803980, objYaw = 180.00001525879, objCoords = vec3(469.927368, -1000.543701, 26.405483)},
+        {objName = -288803980, objYaw = 0.0, objCoords = vec3(467.522217, -1000.543701, 26.405483)}
+    },
+    locked = true,
+}
+
+-- pddoor11 created by JJ_Case
+Config.DoorList['police-pddoor11'] = {
+    authorizedJobs = { ['police'] = 0 },
+    doorType = 'double',
+    doorRate = 1.0,
+    doorLabel = 'pddoor11',
+    distance = 3,
+    doors = {
+        {objName = -692649124, objYaw = 0.0, objCoords = vec3(467.368622, -1014.406006, 26.483816)},
+        {objName = -692649124, objYaw = 180.00001525879, objCoords = vec3(469.774261, -1014.406006, 26.483816)}
+    },
+    locked = true,
+}
+
+-- pddoor12 created by JJ_Case
+Config.DoorList['police-pddoor12'] = {
+    objCoords = vec3(477.912598, -1012.188660, 26.480055),
+    distance = 3,
+    objName = -53345114,
+    authorizedJobs = { ['police'] = 0 },
+    doorType = 'door',
+    doorRate = 1.0,
+    objYaw = 0.0,
+    fixText = false,
+    locked = true,
+    doorLabel = 'pddoor12',
+}
+
+-- pddoor13 created by JJ_Case
+Config.DoorList['police-pddoor13'] = {
+    objCoords = vec3(476.615692, -1008.875427, 26.480055),
+    distance = 3,
+    objName = -53345114,
+    authorizedJobs = { ['police'] = 0 },
+    doorType = 'door',
+    doorRate = 1.0,
+    objYaw = 270.13998413086,
+    fixText = false,
+    locked = true,
+    doorLabel = 'pddoor13',
+}
+
+-- pddoor14 created by JJ_Case
+Config.DoorList['police-pddoor14'] = {
+    objCoords = vec3(486.913116, -1012.188660, 26.480055),
+    distance = 3,
+    objName = -53345114,
+    authorizedJobs = { ['police'] = 0 },
+    doorType = 'door',
+    doorRate = 1.0,
+    objYaw = 0.0,
+    fixText = false,
+    locked = true,
+    doorLabel = 'pddoor14',
+}
+
+-- pddoor15 created by JJ_Case
+Config.DoorList['police-pddoor15'] = {
+    objCoords = vec3(483.912720, -1012.188660, 26.480055),
+    distance = 3,
+    objName = -53345114,
+    authorizedJobs = { ['police'] = 0 },
+    doorType = 'door',
+    doorRate = 1.0,
+    objYaw = 0.0,
+    fixText = false,
+    locked = true,
+    doorLabel = 'pddoor15',
+}
+
+-- pddoor16 created by JJ_Case
+Config.DoorList['police-pddoor16'] = {
+    objCoords = vec3(480.912811, -1012.188660, 26.480055),
+    distance = 3,
+    objName = -53345114,
+    authorizedJobs = { ['police'] = 0 },
+    doorType = 'door',
+    doorRate = 1.0,
+    objYaw = 0.0,
+    fixText = false,
+    locked = true,
+    doorLabel = 'pddoor16',
+}
+
+-- pddoor17 created by JJ_Case
+Config.DoorList['police-pddoor17'] = {
+    objCoords = vec3(484.176422, -1007.734375, 26.480055),
+    distance = 3,
+    objName = -53345114,
+    authorizedJobs = { ['police'] = 0 },
+    doorType = 'door',
+    doorRate = 1.0,
+    objYaw = 180.00001525879,
+    fixText = false,
+    locked = true,
+    doorLabel = 'pddoor17',
+}
+
+-- pddoor18 created by JJ_Case
+Config.DoorList['police-pddoor18'] = {
+    objCoords = vec3(481.008362, -1004.117981, 26.480055),
+    distance = 3,
+    objName = -53345114,
+    authorizedJobs = { ['police'] = 0 },
+    doorType = 'door',
+    doorRate = 1.0,
+    objYaw = 180.00001525879,
+    fixText = false,
+    locked = true,
+    doorLabel = 'pddoor18',
+}
+
+-- pddoor19 created by JJ_Case
+Config.DoorList['police-pddoor19'] = {
+    objCoords = vec3(479.059967, -1003.172974, 26.406504),
+    distance = 3,
+    objName = -288803980,
+    authorizedJobs = { ['police'] = 0 },
+    doorType = 'door',
+    doorRate = 1.0,
+    objYaw = 89.999977111816,
+    fixText = false,
+    locked = true,
+    doorLabel = 'pddoor19',
+}
+
+-- pddoor20 created by JJ_Case
+Config.DoorList['police-pddoor20'] = {
+    authorizedJobs = { ['police'] = 0 },
+    doorType = 'double',
+    doorRate = 1.0,
+    doorLabel = 'pddoor20',
+    distance = 3,
+    doors = {
+        {objName = 149284793, objYaw = 0.0, objCoords = vec3(479.663757, -997.909973, 26.406504)},
+        {objName = 149284793, objYaw = 180.00001525879, objCoords = vec3(482.068573, -997.909973, 26.406504)}
+    },
+    locked = true,
+}
+
+-- pddoor21 created by JJ_Case
+Config.DoorList['police-pddoor21'] = {
+    objCoords = vec3(482.670258, -995.728516, 26.405483),
+    distance = 3,
+    objName = -1406685646,
+    authorizedJobs = { ['police'] = 0 },
+    doorType = 'door',
+    doorRate = 1.0,
+    objYaw = 270.00003051758,
+    fixText = false,
+    locked = true,
+    doorLabel = 'pddoor21',
+}
+
+-- pddoor22 created by JJ_Case
+Config.DoorList['police-pddoor22'] = {
+    objCoords = vec3(482.670135, -987.579163, 26.405483),
+    distance = 3,
+    objName = -1406685646,
+    authorizedJobs = { ['police'] = 0 },
+    doorType = 'door',
+    doorRate = 1.0,
+    objYaw = 270.00003051758,
+    fixText = false,
+    locked = true,
+    doorLabel = 'pddoor22',
+}
+
+-- pddoor23 created by JJ_Case
+Config.DoorList['police-pddoor23'] = {
+    authorizedJobs = { ['police'] = 0 },
+    doorType = 'double',
+    doorRate = 1.0,
+    doorLabel = 'pddoor23',
+    distance = 3,
+    doors = {
+        {objName = 149284793, objYaw = 89.999977111816, objCoords = vec3(479.062408, -987.437561, 26.405483)},
+        {objName = 149284793, objYaw = 270.00003051758, objCoords = vec3(479.062408, -985.032349, 26.405483)}
+    },
+    locked = true,
+}
+
+-- pddoor24 created by JJ_Case
+Config.DoorList['police-pddoor24'] = {
+    objCoords = vec3(475.832336, -990.483948, 26.405483),
+    distance = 3,
+    objName = -692649124,
+    authorizedJobs = { ['police'] = 0 },
+    doorType = 'door',
+    doorRate = 1.0,
+    objYaw = 134.97177124023,
+    fixText = false,
+    locked = true,
+    doorLabel = 'pddoor24',
+}
+
+-- pddoor25 created by JJ_Case
+Config.DoorList['police-pddoor25'] = {
+    objCoords = vec3(458.654327, -990.649780, 30.823193),
+    distance = 3,
+    objName = -96679321,
+    authorizedJobs = { ['police'] = 0 },
+    doorType = 'door',
+    doorRate = 1.0,
+    objYaw = 270.00003051758,
+    fixText = false,
+    locked = true,
+    doorLabel = 'pddoor25',
+}
+
+-- pddoor26 created by JJ_Case
+Config.DoorList['police-pddoor26'] = {
+    authorizedJobs = { ['police'] = 0 },
+    doorType = 'double',
+    doorRate = 1.0,
+    doorLabel = 'pddoor26',
+    distance = 3,
+    doors = {
+        {objName = -1547307588, objYaw = 180.00001525879, objCoords = vec3(458.208740, -972.254272, 30.815308)},
+        {objName = -1547307588, objYaw = 0.0, objCoords = vec3(455.886169, -972.254272, 30.815308)}
+    },
+    locked = true,
+}
+
+-- pddoor27 created by JJ_Case
+Config.DoorList['police-pddoor27'] = {
+    objCoords = vec3(464.308563, -984.528442, 43.771240),
+    distance = 3,
+    objName = -692649124,
+    authorizedJobs = { ['police'] = 0 },
+    doorType = 'door',
+    doorRate = 1.0,
+    objYaw = 89.999977111816,
+    fixText = false,
+    locked = true,
+    doorLabel = 'pddoor27',
+}
