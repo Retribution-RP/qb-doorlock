1f8bdeaca74dba25da0a486a9428612d5726c7e8
jj_case-gaming <jjcasegaming@yahoo.com>
2023-01-09 20:57:18 -0800

    added bcso locks

---
+++ configs/bcso.lua
@@ -0,0 +1,225 @@
+
+
+-- bcso1 created by JJ_Case
+Config.DoorList['bcso-bcso1'] = {
+    locked = true,
+    distance = 2,
+    objName = 1364638935,
+    doorRate = 1.0,
+    doorLabel = 'bcso1',
+    fixText = false,
+    objCoords = vec3(1838.006104, 3677.104004, 34.282230),
+    authorizedJobs = { ['bcso'] = 0 },
+    objYaw = 120.0,
+    doorType = 'door',
+}
+
+-- bcso2 created by JJ_Case
+Config.DoorList['bcso-bcso2'] = {
+    locked = true,
+    distance = 2,
+    objName = -1264811159,
+    doorRate = 1.0,
+    doorLabel = 'bcso2',
+    fixText = false,
+    objCoords = vec3(1838.965210, 3682.856689, 34.282230),
+    authorizedJobs = { ['bcso'] = 0 },
+    objYaw = 29.999982833862,
+    doorType = 'door',
+}
+
+-- bcso3 created by JJ_Case
+Config.DoorList['bcso-bcso3'] = {
+    locked = true,
+    distance = 2,
+    objName = -1264811159,
+    doorRate = 1.0,
+    doorLabel = 'bcso3',
+    fixText = false,
+    objCoords = vec3(1829.852783, 3673.788818, 34.282230),
+    authorizedJobs = { ['bcso'] = 0 },
+    objYaw = 300.00003051758,
+    doorType = 'door',
+}
+
+-- bcso4 created by JJ_Case
+Config.DoorList['bcso-bcso4'] = {
+    locked = true,
+    distance = 2,
+    objName = -1264811159,
+    doorRate = 1.0,
+    doorLabel = 'bcso4',
+    fixText = false,
+    objCoords = vec3(1830.650146, 3676.562256, 34.282230),
+    authorizedJobs = { ['bcso'] = 0 },
+    objYaw = 210.00004577637,
+    doorType = 'door',
+}
+
+-- bcso5 created by JJ_Case
+Config.DoorList['bcso-bcso5'] = {
+    locked = true,
+    distance = 2,
+    objName = -1501157055,
+    doorRate = 1.0,
+    doorLabel = 'bcso5',
+    fixText = false,
+    objCoords = vec3(1823.863281, 3681.116943, 34.339008),
+    authorizedJobs = { ['bcso'] = 0 },
+    objYaw = 29.999982833862,
+    doorType = 'door',
+}
+
+-- bcso6 created by JJ_Case
+Config.DoorList['bcso-bcso6'] = {
+    locked = true,
+    distance = 2,
+    objName = -1264811159,
+    doorRate = 1.0,
+    doorLabel = 'bcso6',
+    fixText = false,
+    objCoords = vec3(1829.384277, 3680.266846, 34.282230),
+    authorizedJobs = { ['bcso'] = 0 },
+    objYaw = 120.00000762939,
+    doorType = 'door',
+}
+
+-- bcso7 created by JJ_Case
+Config.DoorList['bcso-bcso7'] = {
+    locked = true,
+    distance = 2,
+    objName = 1364638935,
+    doorRate = 1.0,
+    doorLabel = 'bcso7',
+    fixText = false,
+    objCoords = vec3(1812.306641, 3672.724365, 34.282230),
+    authorizedJobs = { ['bcso'] = 0 },
+    objYaw = 300.00003051758,
+    doorType = 'door',
+}
+
+-- bcso8 created by JJ_Case
+Config.DoorList['bcso-bcso8'] = {
+    locked = true,
+    distance = 2,
+    objName = 2010487154,
+    doorRate = 1.0,
+    doorLabel = 'bcso8',
+    fixText = false,
+    objCoords = vec3(1813.551270, 3675.055420, 34.396088),
+    authorizedJobs = { ['bcso'] = 0 },
+    objYaw = 29.999982833862,
+    doorType = 'door',
+}
+
+-- bcso9 created by JJ_Case
+Config.DoorList['bcso-bcso9'] = {
+    locked = true,
+    distance = 2,
+    objName = 2010487154,
+    doorRate = 1.0,
+    doorLabel = 'bcso9',
+    fixText = false,
+    objCoords = vec3(1807.131104, 3681.660400, 34.396088),
+    authorizedJobs = { ['bcso'] = 0 },
+    objYaw = 120.00000762939,
+    doorType = 'door',
+}
+
+-- bcso10 created by JJ_Case
+Config.DoorList['bcso-bcso10'] = {
+    locked = true,
+    distance = 2,
+    objName = 2010487154,
+    doorRate = 1.0,
+    doorLabel = 'bcso10',
+    fixText = false,
+    objCoords = vec3(1808.629395, 3679.065430, 34.396088),
+    authorizedJobs = { ['bcso'] = 0 },
+    objYaw = 120.00000762939,
+    doorType = 'door',
+}
+
+-- bcso11 created by JJ_Case
+Config.DoorList['bcso-bcso11'] = {
+    locked = true,
+    distance = 2,
+    objName = 2010487154,
+    doorRate = 1.0,
+    doorLabel = 'bcso11',
+    fixText = false,
+    objCoords = vec3(1810.131104, 3676.464355, 34.396088),
+    authorizedJobs = { ['bcso'] = 0 },
+    objYaw = 120.00000762939,
+    doorType = 'door',
+}
+
+-- bcso12 created by JJ_Case
+Config.DoorList['bcso-bcso12'] = {
+    locked = true,
+    distance = 2,
+    objName = -1264811159,
+    doorRate = 1.0,
+    doorLabel = 'bcso12',
+    fixText = false,
+    objCoords = vec3(1828.527344, 3680.228760, 38.952450),
+    authorizedJobs = { ['bcso'] = 0 },
+    objYaw = 300.00003051758,
+    doorType = 'door',
+}
+
+-- bcso13 created by JJ_Case
+Config.DoorList['bcso-bcso13'] = {
+    locked = true,
+    distance = 2,
+    objName = -1626613696,
+    doorRate = 1.0,
+    doorLabel = 'bcso13',
+    fixText = false,
+    objCoords = vec3(1828.429321, 3673.811768, 38.952450),
+    authorizedJobs = { ['bcso'] = 0 },
+    objYaw = 120.00000762939,
+    doorType = 'door',
+}
+
+-- bcso14 created by JJ_Case
+Config.DoorList['bcso-bcso14'] = {
+    locked = true,
+    distance = 2,
+    objName = -1626613696,
+    doorRate = 1.0,
+    doorLabel = 'bcso14',
+    fixText = false,
+    objCoords = vec3(1831.229858, 3675.428467, 38.952450),
+    authorizedJobs = { ['bcso'] = 0 },
+    objYaw = 120.0,
+    doorType = 'door',
+}
+
+-- bcso15 created by JJ_Case
+Config.DoorList['bcso-bcso15'] = {
+    locked = true,
+    distance = 2,
+    objName = -1156020871,
+    doorRate = 1.0,
+    doorLabel = 'bcso15',
+    fixText = false,
+    objCoords = vec3(1845.406982, 3677.485107, 34.611950),
+    authorizedJobs = { ['bcso'] = 0 },
+    objYaw = 30.000001907349,
+    doorType = 'door',
+}
+
+-- bcso16 created by JJ_Case
+Config.DoorList['bcso-bcso16'] = {
+    locked = true,
+    distance = 2,
+    objName = 1286535678,
+    doorRate = 1.0,
+    doorLabel = 'bcso16',
+    fixText = false,
+    objCoords = vec3(1862.001953, 3687.521973, 33.015141),
+    authorizedJobs = { ['bcso'] = 0 },
+    objYaw = 30.000005722046,
+    doorType = 'sliding',
+}
